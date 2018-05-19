f2a84c47b72040f8f0f4b64d89c53b446a1ab5c8
saifdev1642 <saifdev1642@gmail.com>
2018-05-20 03:31:02 +0600

    Access control

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        Laravelbiz
                     </a>
                 </div>
 
